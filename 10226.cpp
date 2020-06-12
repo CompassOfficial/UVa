577c0db17c0606c57d35ea6d7897a41e34b5b15e
CompassOfficial <64495528+CompassOfficial@users.noreply.github.com>
2020-06-12 18:13:29 +0600

    Create 10226.cpp

---
+++ 10226.cpp
@@ -0,0 +1,38 @@
+  /*  HARD WORK FOREVER PAYS  */
+#include<bits/stdc++.h>
+#define pb push_back
+#define mk make_pair
+#define f first
+#define s second
+#define turbo(){	\
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL); \
+}
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PII;
+int main() 
+{ 
+	turbo();
+	int t;
+	cin>>t;
+	while(t--){
+		double tot=0;
+		string s;
+		map<string,int> mp;
+		while(getline(cin,s)){
+			if(s.empty()&& tot==0) continue;
+			else if(s.empty()) break;
+			mp[s]++;
+			tot++;
+			
+		}
+		for(auto u:mp){
+			cout<<u.f<<" "<<fixed<<setprecision(4)<<(u.s/tot)*100<<"\n";
+		}
+		if(t>0){
+			cout<<"\n";
+		}
+	}
+    return 0; 
+} 
