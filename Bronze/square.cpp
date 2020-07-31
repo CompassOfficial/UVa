6efabc9d43c006d80eb1210216f079561a779cf9
CompassOfficial <64495528+CompassOfficial@users.noreply.github.com>
2020-07-31 22:10:48 +0600

    Update square.cpp

--- Bronze/square.cpp
+++ Bronze/square.cpp
@@ -1,1 +1,24 @@
-
+/*
+ID: thauhid2
+TASK: 
+LANG: C++11
+*/
+#include<bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PII;
+int main()
+{
+	freopen("square.in", "r", stdin);
+	freopen("square.out", "w", stdout);
+	int x1,x2,y1,y2,x_1,x_2,y_1,y_2,ans=0,b,a;
+	cin>>x1>>y1>>x2>>y2>>x_1>>y_1>>x_2>>y_2;
+	a=abs(max({x1,x2,x_1,x_2})-min({x1,x2,x_1,x_2}));
+	b=abs(max({y1,y2,y_1,y_2})-min({y1,y2,y_1,y_2}));
+	ans=max(a,b);
+	cout<<ans*ans<<"\n";
+	return 0;
+}
